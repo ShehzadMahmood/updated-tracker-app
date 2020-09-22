d050d98327100fc3cae2dfcad88e3cb8dc7a4b76
Shehzad Mahmood <65547678+ShehzadMahmood@users.noreply.github.com>
2020-09-23 02:29:20 +0500

    updated-tracker-app

---
+++ build/precache-manifest.98b5a0f4eb9f79e8b5927236433f9b7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92ebb7c2936fb70d76dd8b7d22c6755",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd3b13849ea71a5d5656",
+    "url": "/static/css/main.489d9dfd.chunk.css"
+  },
+  {
+    "revision": "e6947362c335ed133971",
+    "url": "/static/js/2.7454d7af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7454d7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3b13849ea71a5d5656",
+    "url": "/static/js/main.a7686cad.chunk.js"
+  },
+  {
+    "revision": "eebd3b613403979bdb7d",
+    "url": "/static/js/runtime-main.beca9557.js"
+  }
+]);
